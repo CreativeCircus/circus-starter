9ca7bb3145f64e62c06ba41e8d8f342cb55b85b7
Christopher Silich <chrissilich@gmail.com>
2018-03-30 00:27:49 -0400

    gulpfile rewrite, eslint, htmllint, imagemin, friendly messaging
    gulpfile reworked to use gulp-watch (not gulp.watch)
    switched to eslint
    added htmllint
    added imagemin with changed
    added nice messages after all file changes/conversions

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,167 @@
 // all the node modules we need
 const gulp = require('gulp'),
+	watch = require('gulp-watch'),
 	sass = require('gulp-sass'),
 	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps'),
 	babel = require('gulp-babel'),
 	eslint = require('gulp-eslint'),
-	stylish = require('jshint-stylish'),
-	newer = require('gulp-newer'),
+	changed = require('gulp-changed'),
 	imagemin = require('gulp-imagemin'),
 	htmllint = require('gulp-htmllint'),
 	fancyLog = require('fancy-log'),
-	colors = require('ansi-colors'),
+	colors = require('colors'),
+	plumber = require('gulp-plumber'),
+	gulpFn  = require('gulp-fn'),
 	browserSync = require('browser-sync').create()
 
 
 
 gulp.task('html-check', function() {
-	return gulp.src(['!./node_modules/**/*.html', './**/*.html'])
-		.pipe(htmllint({}, htmllintReporter));
+	return watch('*.html', { ignoreInitial: false })
+		.pipe(plumber())
+		.pipe(htmllint({}, htmllintReporter))
 });
-
-
 
 var htmllintReporter = function (filepath, issues) {
 	var filepathSplit = filepath.split("/");
 	filename = filepathSplit[filepathSplit.length - 1];
 	if (issues.length > 0) {
-		// fancyLog(colors.cyan('[gulp-htmllint] '));
 		issues.forEach(function (issue) {
-			fancyLog("HTML Issue: " + colors.white(filename + ', line ' + issue.line + ': ') + colors.red(issue.msg));
+			fancyLog("HTML Issue: " + filename + ', line ' + issue.line + ': '.white + issue.msg.red);
 		});
  
 		process.exitCode = 1;
+	} else {
+		console.log("HTML checked for errors: ".cyan);
+		console.log(filepath);
 	}
 }
 	
 
+
 // this task takes sass files and compiles them
 gulp.task('sass-compile', function () {
-	return gulp.src(['./src/scss/**/_*.scss', './src/scss/**/*.scss']) // run over these files
+	return watch('src/scss/**/*.scss') // run over these files
+		.pipe(plumber())
 		.pipe(sourcemaps.init()) // make sourcemaps for chrome devtools
 		.pipe(sass({ // convert the sass into plain css
 			outputStyle: 'expanded',  // change to 'expanded' to make it readable
-			indentType: "tab", 
+			indentType: 'tab', 
 			indentWidth: 1,
 		}).on('error', sass.logError)) // on errors, show them
 		.pipe(sourcemaps.write('./')) // put the sourcemaps with the css files
-		.pipe(gulp.dest('./dist/css')) // put the css files here.
+		.pipe(gulp.dest((file) => file.base.replace('src', 'dist').replace('scss', 'css'))) // put the css files here.
 		.pipe(browserSync.stream()) // tell browsersync to send over the changes
+		.pipe(gulpFn(function(file) {
+			if (file.path.indexOf('.css.map') == -1) {
+				console.log("SCSS converted to CSS: ".cyan);
+				console.log(file.path.replace('src', 'dist').replace('scss', 'css'))
+			}
+		}))
 });
+
+
+// this task looks through js files for errors
+gulp.task('js-check', function () {
+	// if eslint complains about a certain error, and you want to turn it off, or change it
+	// go here for the rules https://eslint.org/docs/rules/
+	// and you'll need to change them in the .eslintrc file in the root of the project.
+	return watch(['src/js/**/*.js', 'dist/js/**/*.js'], function() {
+		return gulp.src(['src/js/**/*.js', 'dist/js/**/*.js'])
+			.pipe(plumber())
+			.pipe(eslint())
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError())	
+	})
+})
 
 
 
 // this task looks through js files for errors
 gulp.task('js-compile', function () {
-	return gulp.src('./src/js/**/*.js') // watch these files
+	return watch('src/js/**/*.js') // watch these files
+		.pipe(plumber())
 		.pipe(sourcemaps.init()) // make sourcemaps for chrome devtools
 		.pipe(babel({ // run the js through babel to convert ES6 to ES5
 			presets: ['env'],
 		}))
 		.on('error', function (err) {
-			console.warn(err.message);
+			console.warn('[JS Babel Error] '.red + err.message);
 			this.emit('end');
-		})		
+		})
 		// .pipe(concat('./app.js')) // join all the js files into one // uncomment this line if you want to concatenate all JS files into one.
 		.pipe(sourcemaps.write('./')) // put the sourcemaps with the js files
-		.pipe(gulp.dest('./dist/js')) // put the js files here.
+		.pipe(gulp.dest((file) => file.base.replace('src', 'dist'))) // put the js files here.
 		.pipe(browserSync.stream()) // tell browsersync to send over the changes
+		.pipe(gulpFn(function(file) {
+			if (file.path.indexOf('.js.map') == -1) {
+				console.log("JS generated: ".cyan);
+				console.log(file.path.replace('src', 'dist'));
+			}
+		}))
 });
 
 // this task looks through js files for errors
 gulp.task('image-compress', function () {
-	var imgDest = './dist/img';
-	return gulp.src('./src/img/**/*') // watch these files
-		.pipe(newer(imgDest))
+	let imgDest = 'dist/img/';
+	return watch('src/img/*.*') // watch these files
+		.pipe(plumber())
+		.pipe(changed(imgDest))
 		.pipe(imagemin())
-		.pipe(gulp.dest(imgDest))
+		.pipe(gulp.dest(imgDest)) // put the image files here.
+		.pipe(browserSync.stream()) // tell browsersync to send over the changes
+		.pipe(gulpFn(function(file) {
+			console.log("Image compressed and copied to: ".cyan);
+			console.log(file.history[1])
+		}))
+})
+
+gulp.task('welcome', function () {
+	console.log(colors.red('Starting Circus Starter template gulpfile! Wizz, whirrrrr, bang, pop!'));
+})
+
+gulp.task('make-cool-shit', function () {
+	setTimeout(() => {
+		console.log(' ');
+		console.log(' ================================================ '.red);
+		console.log('  Welcome to the Circus Starter template.         '.white.bold);
+		console.log('  You\'re good to go.                              '.white.bold);
+		console.log('  Make cool 💩.                                     '.white.bold);
+		console.log('                                - Chris Silich    '.white.dim);
+		console.log(' ================================================ '.red);
+		console.log(' ');
+	}, 500)
 })
 
 
-// this task looks through js files for errors
-gulp.task('js-check', function () {
-	return gulp.src(['!./dist/js/app.js', './src/js/**/*.js', './dist/js/**/*.js']) // watch these files
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
-})
 
-
-gulp.task('default', function() { // running `gulp` runs this task. this task sort of branches off into the others as needed
-
+gulp.task('start-browsersync', function() {
 	browserSync.init({ // start the browsersync mini-server
 		server: "./", // on the root of the project
 	});
+})
 
-	gulp.watch(['!./node_modules/*.*', './**/*.html'], ['html-check']); // watch html files. if they change, run the task called html-check
-	gulp.watch('./src/img/**/', ['image-compress']); // watch image files. if they change, run the task called image-compress
-	gulp.watch('./src/scss/**/*.scss', ['sass-compile']); // watch sass files. if they change, run the task called sass-compile
-	gulp.watch('./src/js/**/*.js', ['js-compile']); // watch js files. if they change, run the tasks called js-check and js-compile
-	gulp.watch('./dist/js/**/*.js', ['js-check']); // watch js files. if they change, run the tasks called js-check
-	gulp.watch('./**/*.html').on('change', browserSync.reload); // watch all top level files and reload if they change
-});
+
+ // running `gulp` runs this task. this task sort of branches off into the others as needed
+gulp.task('default', [
+	'welcome', 
+	'html-check', 
+	'image-compress', 
+	'js-check', 
+	'js-compile', 
+	'sass-compile', 
+	'start-browsersync', 
+	'make-cool-shit'
+]);
+
+gulp.task('no-browser-sync', [
+	'welcome', 
+	'html-check', 
+	'image-compress', 
+	'js-check', 
+	'js-compile', 
+	'sass-compile', 
+	// 'start-browsersync', 
+	'make-cool-shit'
+]);
