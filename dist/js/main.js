2fc0da70cf2dd9595b0a0667796dfbee2bc2bd41
Christopher Silich <chrissilich@gmail.com>
2018-04-05 15:06:21 -0400

    oops, commited classwork

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 "use strict";
 
 console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
-
