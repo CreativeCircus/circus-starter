38a6490c391b15440786eef3ae793e91d3bed4c3
Christopher Silich <chrissilich@gmail.com>
2018-04-29 14:50:29 -0400

    testing something

--- gulpfile.js
+++ gulpfile.js
@@ -186,9 +186,9 @@ gulp.task('version', () => {
 	
 		axios.get('https://api.github.com/repos/creativecircus/circus-starter')
 			.then(response => {
-				// console.log(response.data)
 				let remote = response.data.updated_at.substr(0,19);
 				let local = util.inspect(stats.mtime).substr(0,19);
+				console.log('local, remote', local, remote);
 				if (remote <= local) {
 					console.warn('this gulpfile appears to be up to date')
 				} else {
