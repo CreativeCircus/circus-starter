73a7f3559cf8953d8618fcee132824be35d2cf9c
Chris Silich <chris.silich@gmail.com>
2019-04-02 16:22:18 -0400

    rolled back package.json name change, revved version numbers properly

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 const gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	sass = require('gulp-sass'),
-	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer'),
 	babel = require('gulp-babel'),
@@ -15,7 +14,6 @@ const gulp = require('gulp'),
 	gulpFn  = require('gulp-fn'),
 	axios = require('axios'),
 	fs = require('fs'),
-	util = require('util'),
 	browserSync = require('browser-sync').create()
 
 
